6138d994ffe717aaa6839a79b280d5fa13da9af0
Lesyk222 <oleszherebetskyi@gmail.com>
2023-04-17 22:50:30 +0300

    delete sort from function readArray

--- oop_lab5_prog/CMultiSet.cpp
+++ oop_lab5_prog/CMultiSet.cpp
@@ -110,16 +110,6 @@ void CMultiSet::readArray() {
 		cin >> value;
 		addElement(value);
 	}
-
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size - i - 1; j++) {
-			if (values[j] > values[j + 1]) {
-				int temp = values[j];
-				values[j] = values[j + 1];
-				values[j + 1] = temp;
-			}
-		}
-	}
 }
 
 CMultiSet CMultiSet::operator+(const CMultiSet& other) const {
